39dd0c4a5c313568103b66531d9ac1029ce82fe9
C:\Users\vvn\AppData\Roaming\The Bat! <C:\Users\vvn\AppData\Roaming\The Bat!@localhost>
2018-01-23 16:06:43 +0400

    jsoap-gson-object

--- src/main/java/ru/rit/Controllers/MyController.java
+++ src/main/java/ru/rit/Controllers/MyController.java
@@ -1,11 +1,12 @@
 package ru.rit.Controllers;
 
+import com.google.code.stackexchange.schema.Question;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,48 +15,57 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 import com.google.code.stackexchange.client.query.StackExchangeApiQueryFactory;
-import com.google.code.stackexchange.common.PagedList;
 import com.google.code.stackexchange.schema.*;
-import ru.rit.FillQuestions;
-
-
+import ru.rit.QuestionsViaAPI;
+import ru.rit.QuestionsViaJson;
 
 
 @Controller
-//@RequestMapping(value = "/rit")
 public class MyController {
     @GetMapping({"","search"})
-    public String index(Model model, @ModelAttribute(value="tags") String tags, @ModelAttribute(value="pagenumber") String pageNumberStr, @ModelAttribute(value="pagesize") String pageSizeStr) {
+    public String index(Model model, @ModelAttribute(value="tags") String tags,@ModelAttribute(value="mode") String mode,@ModelAttribute(value="pagenumber") String pageNumberStr,@ModelAttribute(value="pagesize") String pageSizeStr) throws IOException {
 
-        if (tags.equals("")) // если параметры поиска не заведены, то выводить нечего
+        if (tags.equals("")) { // если параметры поиска не заведены, то выводить нечего
+            model.addAttribute("errors","Теги не указаны!!!");
             return "index";
-
-        StackExchangeApiQueryFactory queryFactory = StackExchangeApiQueryFactory.newInstance();
-
-        //
-        Paging paging;
-        try {
-            paging = new Paging(Integer.parseInt(pageNumberStr), Integer.parseInt(pageSizeStr));
-        } catch (Exception e) {
-            paging = new Paging();
         }
-        //Paging paging = new Paging(1, 2);
 
-        //вопросы и ответы с телом
-        String filter = "withbody";
+        String errors = null;
 
         // теги для отбора
-        List<String> tagList= Arrays.asList(tags.split(","));
+        List<String> tagList= Arrays.asList(tags.split(";"));
 
-        //получаем список вопросов с авторами и ответами
-        FillQuestions fillQuestions = new FillQuestions(tagList,paging,filter);
-        PagedList<Question> questions = fillQuestions.getQuestions();
+        if (mode.equals("API")) {
+            StackExchangeApiQueryFactory queryFactory = StackExchangeApiQueryFactory.newInstance();
+
+            Paging paging;
+            try {
+                paging = new Paging(Integer.parseInt(pageNumberStr), Integer.parseInt(pageSizeStr));
+            } catch (Exception e) {
+                paging = new Paging();
+            }
+            //Paging paging = new Paging(1, 2);
+
+            //вопросы и ответы с телом
+            String filter = "withbody";
+
+            //получаем список вопросов с авторами и ответами
+            QuestionsViaAPI questionsViaAPI = new QuestionsViaAPI(tagList, paging, filter);
+            List<Question> questions = questionsViaAPI.getQuestions();
+            errors = questionsViaAPI.getErrorStr();
+            model.addAttribute("questions",questions);
+        } else{
+            QuestionsViaJson conn = new QuestionsViaJson(pageNumberStr, pageSizeStr);
+            List<ru.rit.Generated.Question> questions = conn.fillQuestions(tags);
+            errors = conn.getErrorStr();
+            model.addAttribute("questions",questions);
+        }
 
         model.addAttribute("tags",tags);
         model.addAttribute("pagenumber",pageNumberStr);
         model.addAttribute("pagesize",pageSizeStr);
-        model.addAttribute("errors",fillQuestions.getErrorStr());
-        model.addAttribute("questions",questions);
+        model.addAttribute("errors",errors);
+        model.addAttribute("mode",mode);
 
         return "index";
     }
